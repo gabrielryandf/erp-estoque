edf50bcade03a9dcb951f9f1261ea0811c8323cf
luixzss <luixzss2006@gmail.com>
2025-11-18 19:19:31 -0300

    Update main.py

--- Cadastro de itens em estoque/main.py
+++ Cadastro de itens em estoque/main.py
@@ -1,2 +1,11 @@
 from estoque import cadastrar_produto, excluir_produto, mostrar_relatorio
 from database import con  # para fechar depois
+def main():
+    while True:
+        print("\n=== Sistema de Estoque ===")
+        print("1 - Cadastrar produto")
+        print("2 - Excluir produto")
+        print("3 - Mostrar relatório")
+        print("4 - Sair")
+
+        opcao = input("Escolha: ")
